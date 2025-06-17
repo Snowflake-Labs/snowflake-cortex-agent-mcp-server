f96454b713e0b3b964bb8e652c353807d402b192
Kamesh Sampath <kamesh.sampath@hotmail.com>
2025-06-17 17:01:41 +0530

    (feat!): [skip ci]  add demo data setup

--- scripts/pat.sh
+++ scripts/pat.sh
@@ -8,7 +8,7 @@ snow sql --stdin <<EOF
     CREATE USER IF NOT EXISTS $SA_USER
         TYPE = SERVICE
         COMMENT = 'Service User For Cortex Agents MCP Demo';
-    GRANT ROLE $SNOWFLAKE_ROLE TO USER $SA_USER;
+    GRANT ROLE $SNOWFLAKE_MCP_DEMO_ROLE TO USER $SA_USER;
 EOF
 
 # # Get GitHub Actions IP ranges only IPV4
@@ -24,8 +24,8 @@ CIDR_VALUE_LIST="'${LOCAL_IP}'"
 # Create or alter the network rule and policy
 snow sql --query "use role accountadmin;alter user $SA_USER unset network_policy;" || true
 snow sql --stdin <<EOF
-use role $SNOWFLAKE_ROLE;
-use database $SNOWFLAKE_DATABASE;
+use role $SNOWFLAKE_MCP_DEMO_ROLE;
+use database $SNOWFLAKE_MCP_DEMO_DATABASE;
 create schema if not exists networks;
 create schema if not exists policies;
 
@@ -34,9 +34,9 @@ use role accountadmin;
 -- unset and drop the network policy on the user
 alter user "${SA_USER}" unset network_policy;
 drop network policy if exists LOCAL_PAT_NETWORK_POLICY;
-use role $SNOWFLAKE_ROLE;
+use role $SNOWFLAKE_MCP_DEMO_ROLE;
 -- create or replace network rule to allow local machine IPv4 addresses
-create or replace network rule $SNOWFLAKE_DATABASE.networks.pat_local_access_rule
+create or replace network rule $SNOWFLAKE_MCP_DEMO_DATABASE.networks.pat_local_access_rule
   mode = ingress
   type = ipv4
   value_list = ($CIDR_VALUE_LIST)
@@ -44,7 +44,7 @@ create or replace network rule $SNOWFLAKE_DATABASE.networks.pat_local_access_rul
 -- attach the network rule to the network policy
 use role accountadmin;
 create network policy LOCAL_PAT_NETWORK_POLICY
-allowed_network_rule_list = ('"$SNOWFLAKE_DATABASE".networks.pat_local_access_rule')
+allowed_network_rule_list = ('"$SNOWFLAKE_MCP_DEMO_DATABASE".networks.pat_local_access_rule')
 comment = 'Network policy to allow all IPv4 addresses.';
 alter user "${SA_USER}" set network_policy='LOCAL_PAT_NETWORK_POLICY';
 EOF
@@ -52,14 +52,15 @@ EOF
 # Create or alter the authentication policy that will be set to the service user
 snow sql --query "use role accountadmin;alter user $SA_USER unset AUTHENTICATION POLICY;" || true
 snow sql --stdin <<EOF
-create or replace authentication policy $SNOWFLAKE_DATABASE.policies."${USER}_mcp_auth_policy"
+use role $SNOWFLAKE_MCP_DEMO_ROLE;
+create or replace authentication policy $SNOWFLAKE_MCP_DEMO_DATABASE.policies."${USER}_mcp_auth_policy"
   authentication_methods = ('PROGRAMMATIC_ACCESS_TOKEN')
   pat_policy = (
     default_expiry_in_days=15,
     max_expiry_in_days=30,
     network_policy_evaluation = ENFORCED_REQUIRED
   );
- alter user $SA_USER set AUTHENTICATION POLICY $SNOWFLAKE_DATABASE.policies."${USER}_mcp_auth_policy";
+ alter user $SA_USER set AUTHENTICATION POLICY $SNOWFLAKE_MCP_DEMO_DATABASE.policies."${USER}_mcp_auth_policy";
 EOF
 
 # Create PAT for the service user
@@ -72,7 +73,7 @@ if [ -z "$EXISTING_PAT" ]; then
   # Create PAT if it doesn't exist
   echo "Creating new PAT for service user $SA_USER..."
   SNOWFLAKE_SA_PASSWORD=$(snow sql \
-    --query "ALTER USER IF EXISTS $SA_USER ADD PAT mcp_demo ROLE_RESTRICTION = $SNOWFLAKE_ROLE" \
+    --query "ALTER USER IF EXISTS $SA_USER ADD PAT mcp_demo ROLE_RESTRICTION = $SNOWFLAKE_MCP_DEMO_ROLE" \
     --format=json | jq -r '.[] | .token_secret')
 else
   echo "PAT for service user $SA_USER already exists. Rotating PAT..."
